1b5691c6279e8be9e9aa20a21e2b91f4a3702bab
alex <alexhwang01234@gmail.com>
2021-10-19 20:25:01 -0700

    12.  include likePost in server/database

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -69,5 +69,14 @@ export const deletePost = async (req, res) => {
 
 }
 
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id`);
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
+}
+
 
 export default router
