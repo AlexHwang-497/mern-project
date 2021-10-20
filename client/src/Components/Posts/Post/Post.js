980f39f1636a020bf925c96c3a84adb2ac895d0e
alex <alexhwang01234@gmail.com>
2021-10-19 18:09:09 -0700

    3.  udpated form componenents in  Post.js

--- client/src/Components/Posts/Post/Post.js
+++ client/src/Components/Posts/Post/Post.js
@@ -3,16 +3,42 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
 import useStyles from './Styles'
 
-
-const Post = () =>{
+// *<CardMedia className={classes.media} image={post.selectedFile} title={post.title} />; the posts here are taken from props
+// *<Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>; this will tell us on our card like 5min or 5s ago
+// * post.tags.map((tag) => `#${tag} `); we are looping through our tags and putting "#" on them
+const Post = ({post}) =>{
     const classes = useStyles()
     return (
-        <h1>Post</h1>
-        )
-
-}
+        <Card className={classes.card}>
+          <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+          <div className={classes.overlay}>
+            <Typography variant="h6">{post.creator}</Typography>
+            <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+          </div>
+          <div className={classes.overlay2}>
+            <Button style={{ color: 'white' }} size="small" onClick={() => {}}><MoreHorizIcon fontSize="default" /></Button>
+            {/* <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button> */}
+          </div>
+          <div className={classes.details}>
+            <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+            
+          </div>
+          <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+          </CardContent>
+          <CardActions className={classes.cardActions}>
+            <Button size="small" color="primary" onClick={() => {}}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
+            {/* <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button> */}
+            <Button size="small" color="primary" onClick={() => {}}><DeleteIcon fontSize="small" /> Delete</Button>
+            {/* <Button size="small" color="primary" onClick={() => {dispatch(deletePost(post._id))}}><DeleteIcon fontSize="small" /> Delete</Button> */}
+          </CardActions>
+        </Card>
+      );
+    };
 
 export default Post
