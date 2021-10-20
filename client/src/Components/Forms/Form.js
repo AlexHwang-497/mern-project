357fef2131addda2d2d1d0ad467bf6d8ab98b70b
alex <alexhwang01234@gmail.com>
2021-10-19 19:40:08 -0700

    9. included clear() in form.js

--- client/src/Components/Forms/Form.js
+++ client/src/Components/Forms/Form.js
@@ -16,30 +16,24 @@ const Form = ({ currentId, setCurrentId }) => {
     useEffect(() => {
         if (post) setPostData(post);
       }, [post]);
-    
 
-
-
-
-    const handleSubmit = async(e) =>{
-        e.preventDefault()
-        if (currentId) {
-            dispatch(updatePost(currentId, postData));
-            // clear();
-        } else {
-            dispatch(createPost(postData));
-            // clear();
-          }
-
-
-
-        dispatch(createPost(postData))
-
-    }
-
-    const clear = () =>{
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    }
+      
+      const handleSubmit = async(e) =>{
+          e.preventDefault()
+          if (currentId) {
+              dispatch(updatePost(currentId, postData));
+              
+              
+            } else {
+                dispatch(createPost(postData));
+            }
+            clear()
+        }
+        
+        const clear = () =>{
+            setCurrentId(null)
+            setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+        }
 
 // *value for   <TextField name="creator"/>; the value is going to be stored in the state aka postData.creator
     // *this means the data from the post will be stored in the post data object in the state and each object key will be a speicific text field
@@ -52,7 +46,7 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>Creating a Portfolio</Typography>
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value ={postData.creator} onChange={(e)=> setPostData({...postData,creator:e.target.value})}/>
                 <TextField name="title" variant="outlined" label="Title" fullWidth value ={postData.title} onChange={(e)=> setPostData({...postData,title:e.target.value})}/>
                 <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
